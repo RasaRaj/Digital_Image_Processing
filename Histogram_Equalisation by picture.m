076bf85b89a7235f532a2836fc8cc03520dbf6d3
Rasa Raj <110854496+RasaRaj@users.noreply.github.com>
2023-09-23 21:40:40 +0600

    Histogram_Equalisation by picture
    Histogram Equalisation by picture with graph

---
+++ Histogram_Equalisation by picture.m
@@ -0,0 +1,25 @@
+originalImage = imread('Rasa Profile picture.jpg'); 
+
+grayImage = rgb2gray(originalImage);
+
+equalizedImage = histeq(grayImage);
+
+subplot(2, 2, 1);
+imshow(originalImage);
+title('Original Image');
+
+subplot(2, 2, 2);
+imshow(grayImage);
+title('Original Image');
+
+subplot(2, 2, 2);
+imshow(equalizedImage);
+title('Equalized Image');
+
+subplot(2, 2, 3);
+imhist(grayImage);
+title('Histogram of Original Image');
+
+subplot(2, 2, 4);
+imhist(equalizedImage);
+title('Histogram of Equalized Image');
